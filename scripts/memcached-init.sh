af71e1b40925418134688d20042c3ecca381d971
Joe Orton <jorton@apache.org>
2024-05-09 08:30:35 +0000

    Switch to using CentOS Stream 9 for redis/memcached containers.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1917595 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/memcached-init.sh
+++ scripts/memcached-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 DOCKER=${DOCKER:-`which docker 2>/dev/null || which podman 2>/dev/null`}
 ${DOCKER} build -t httpd_memcached - <<EOF
-FROM quay.io/centos/centos:stream8
-RUN yum install -y memcached
+FROM quay.io/centos/centos:stream9
+RUN yum install --setopt=retries=5 --setopt=timeout=30 -y memcached
 CMD /usr/bin/memcached -u memcached -v
 EOF
 ${DOCKER} run -d -p 11211:11211 httpd_memcached
--- scripts/redis-init.sh
+++ scripts/redis-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 DOCKER=${DOCKER:-`which docker 2>/dev/null || which podman 2>/dev/null`}
 ${DOCKER} build -t httpd_redis - <<EOF
-FROM quay.io/centos/centos:stream8
-RUN yum install -y redis
-CMD /usr/bin/redis-server
+FROM quay.io/centos/centos:stream9
+RUN yum install -y valkey
+CMD /usr/bin/valkey-server
 EOF
 ${DOCKER} run -d -p 6379:6379 httpd_redis
