16184c3f74e8e78cb82927a8c319da4a4cc4855b
Eric Covener <covener@apache.org>
2024-06-26 23:44:27 +0000

    remove doubled up content
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1918686 13f79535-47bb-0310-9956-ffa450edef68

--- t/htdocs/modules/cgi/action.sh
+++ t/htdocs/modules/cgi/action.sh
@@ -7,12 +7,3 @@ echo Content-type: ${CT}
 echo Location: ${LOC}
 echo
 echo "this is action.sh"
-#!/bin/sh
-
-CT=$(echo ${QUERY_STRING}|awk -F: '{print $1'})
-LOC=$(echo ${QUERY_STRING}|awk  -F: '{print $2'})
-
-echo Content-type: ${CT}
-echo Location: ${LOC}
-echo
-echo "this is action.sh"
