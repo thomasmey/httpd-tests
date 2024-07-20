3aae197e92a2f42d6d7b1a4a66cd9594592773d6
Rainer Jung <rjung@apache.org>
2024-07-20 10:13:28 +0000

    Fix PHP test failing due to warnings
    git-svn-id: https://svn.apache.org/repos/asf/httpd/test/framework/trunk@1919388 13f79535-47bb-0310-9956-ffa450edef68

--- t/htdocs/security/CAN-2004-0959.php
+++ t/htdocs/security/CAN-2004-0959.php
@@ -1,5 +1,6 @@
 <?php
- if (is_uploaded_file($_FILES['user_file']['tmp_name'])) {
+ if (array_key_exists('user_file', $_FILES) &&
+     is_uploaded_file($_FILES['user_file']['tmp_name'])) {
      print $_FILES['user_file']['name'];
  } else {
      print "FAILED";
